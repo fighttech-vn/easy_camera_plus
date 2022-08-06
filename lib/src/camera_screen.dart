6b003b54f2486d2e09e308a1ec499ca82bd8fa08
HieuTranTrung <hieu.trantrung1204@gmail.com>
2022-08-06 13:15:20 +0700

    feat(Source): add source

---
+++ lib/src/camera_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Stack(children: [
+      Text('test'),
+    ],),);
+  }
+}
