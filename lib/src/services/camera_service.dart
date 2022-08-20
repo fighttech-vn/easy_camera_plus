e2ac6c47985f0a849df6a87dbfc6567e6e108881
HieuTranTrung <hieu.trantrung1204@gmail.com>
2022-08-21 01:43:13 +0700

    feat(Camera): update frame UI

--- lib/src/services/camera_service.dart
+++ lib/src/services/camera_service.dart
@@ -1,1 +1,14 @@
-class CameraService {}
+import 'package:camera/camera.dart';
+
+import '../models/camera_info.dart';
+
+class CameraService {
+  static late CameraInfo _infoModel;
+  static CameraInfo info = _infoModel;
+
+  Future<void> init() async {
+    final camerasDesc = await availableCameras();
+
+    _infoModel = CameraInfo(camerasDesc: camerasDesc);
+  }
+}
--- lib/src/widgets/frame_layout.dart
+++ lib/src/widgets/frame_layout.dart
@@ -1,16 +1,21 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../models/camera_info.dart';
+import 'camera_video_button.dart';
 import 'capture_camera_button.dart';
 
 class FrameLayoutWidget extends StatefulWidget {
   final Widget child;
   final Function onTakePhoto;
+  final CameraType cameraType;
 
   const FrameLayoutWidget({
     Key? key,
     required this.child,
     required this.onTakePhoto,
+    required this.cameraType,
   }) : super(key: key);
 
   @override
@@ -61,98 +66,157 @@ class _FrameLayoutWidgetState extends State<FrameLayoutWidget> {
   //     ],
   //   );
   // }
+  late CameraType cameraType;
 
   void _onTapTake() {
     widget.onTakePhoto();
   }
 
   @override
+  void initState() {
+    cameraType = widget.cameraType;
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        widget.child,
-        Align(
-          alignment: Alignment.center,
-          child: SvgPicture.asset(
-            'assets/frame_preview_card.svg',
-            package: 'easy_camera_plus',
+    return Material(
+      color: Colors.black,
+      child: Stack(
+        children: <Widget>[
+          widget.child,
+          Align(
+            alignment: Alignment.center,
+            child: SvgPicture.asset(
+              'assets/frame_preview_card.svg',
+              package: 'easy_camera_plus',
+            ),
           ),
-        ),
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: SafeArea(
-            minimum: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
-            child: Align(
-              alignment: Alignment.bottomCenter,
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: Navigator.of(context).pop,
-                    child: Container(
-                      width: 32,
-                      height: 32,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        border: Border.all(color: Colors.white),
-                      ),
-                      child: const Icon(
-                        Icons.close,
-                        color: Colors.white,
-                        size: 22,
-                      ),
-                    ),
-                  ),
-                  CaptureCameraButton(
-                    isDisabled: false,
-                    onTap: _onTapTake,
-                  ),
-                  // _cameraTogglesRowWidget(),
-                  // _captureControlRowWidget(),
-                  GestureDetector(
-                    onTap: Navigator.of(context).pop,
-                    child: Container(
-                      width: 32,
-                      height: 32,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(16),
-                        border: Border.all(color: Colors.white),
-                      ),
-                      child: const Icon(
-                        Icons.flash_on,
-                        color: Colors.white,
-                        size: 22,
-                      ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SafeArea(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    color: const Color(0xFF1A2B61),
+                    alignment: Alignment.bottomCenter,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 25, vertical: 12),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: CameraType.values
+                              .map((e) => GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        cameraType = e;
+                                      });
+                                    },
+                                    child: Container(
+                                      constraints: const BoxConstraints(
+                                          minWidth: 88, minHeight: 33),
+                                      decoration: cameraType != e
+                                          ? null
+                                          : BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(18),
+                                              border: Border.all(
+                                                  color: Colors.white)),
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        e.name.toUpperCase(),
+                                        style: Theme.of(context)
+                                            .textTheme
+                                            .caption
+                                            ?.copyWith(
+                                              color: Colors.white,
+                                              fontSize: 13,
+                                              letterSpacing: -0.3,
+                                            ),
+                                        textAlign: TextAlign.center,
+                                      ),
+                                    ),
+                                  ))
+                              .toList(),
+                        ),
+                        const SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            SizedBox(
+                              width: 50,
+                              child: GestureDetector(
+                                onTap: Navigator.of(context).pop,
+                                child: Text(
+                                  'Cancel',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .caption
+                                      ?.copyWith(
+                                          color: Colors.white, fontSize: 16),
+                                ),
+                              ),
+                            ),
+                            AnimatedCrossFade(
+                              duration: const Duration(milliseconds: 300),
+                              firstChild: CaptureCameraButton(
+                                isDisabled: false,
+                                onTap: _onTapTake,
+                              ),
+                              secondChild: const CameraVideoButton(),
+                              crossFadeState: cameraType == CameraType.photo
+                                  ? CrossFadeState.showFirst
+                                  : CrossFadeState.showSecond,
+                            ),
+                            SizedBox(
+                              width: 50,
+                              child: GestureDetector(
+                                onTap: Navigator.of(context).pop,
+                                child: const Icon(
+                                  CupertinoIcons.camera_rotate,
+                                  color: Colors.white,
+                                  size: 22,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 14),
+                      ],
                     ),
                   ),
                 ],
               ),
             ),
           ),
-        ),
-        // Padding(
-        //   padding: const EdgeInsets.symmetric(horizontal: 24).copyWith(
-        //     top: MediaQuery.of(context).padding.top + 24,
-        //   ),
-        //   child: GestureDetector(
-        //     onTap: Navigator.of(context).pop,
-        //     child: Container(
-        //       width: 32,
-        //       height: 32,
-        //       decoration: BoxDecoration(
-        //         borderRadius: BorderRadius.circular(16),
-        //         border: Border.all(color: Colors.white),
-        //       ),
-        //       child: const Icon(
-        //         Icons.close,
-        //         color: Colors.white,
-        //         size: 26,
-        //       ),
-        //     ),
-        //   ),
-        // ),
-      ],
+          // Padding(
+          //   padding: const EdgeInsets.symmetric(horizontal: 24).copyWith(
+          //     top: MediaQuery.of(context).padding.top + 24,
+          //   ),
+          //   child: GestureDetector(
+          //     onTap: Navigator.of(context).pop,
+          //     child: Container(
+          //       width: 32,
+          //       height: 32,
+          //       decoration: BoxDecoration(
+          //         borderRadius: BorderRadius.circular(16),
+          //         border: Border.all(color: Colors.white),
+          //       ),
+          //       child: const Icon(
+          //         Icons.close,
+          //         color: Colors.white,
+          //         size: 26,
+          //       ),
+          //     ),
+          //   ),
+          // ),
+        ],
+      ),
     );
   }
 }
